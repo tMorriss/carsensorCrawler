ff9d3513fba56e54940243a2ce2397e43fd016e5
tMorriss <tmorriss@tmorriss.com>
2022-12-22 21:05:01 +0900

    fix add sql

--- db.py
+++ db.py
@@ -5,15 +5,6 @@ from mysql import connector
 DATE_FORMAT = '%Y-%m-%d'
 
 
-class CarData:
-    def __init__(self, date: date, name: str, price: int, model_year: int, mileage: int) -> None:
-        self.date = date
-        self.name = name
-        self.price = price
-        self.model_year = model_year
-        self.mileage = mileage
-
-
 class DBConnector:
     def __init__(self, host: str, user: str, pw: str, name: str) -> None:
         # sql接続
@@ -27,11 +18,9 @@ class DBConnector:
         self.cur = self.con.cursor()
         self.con.autocommit = False
 
-    def add(self, data: list[CarData]) -> None:
-        values: list[CarData] = []
-        for d in data:
-            values.append('("{}", "{}", "{}", "{}", "{}")'.format(d.date.strftime(DATE_FORMAT), d.name, d.price, d.model_year, d.mileage))
-        sql = 'INSERT INTO log (date, name, price, year, mileage) VALUES' + ','.join(values)
+    def add(self, date: date, name: str, price: int, model_year: int, mileage: int) -> None:
+        sql = 'INSERT INTO log (date, name, price, year, mileage) VALUES ("{}", "{}", "{}", "{}", "{}")'.format(
+            date.strftime(DATE_FORMAT), name, price, model_year, mileage)
         self.cur.execute(sql)
 
     def delete(self, date: date) -> None:
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from datetime import date
 
 import db_secrets as secrets
-from db import DBConnector, CarData
+from db import DBConnector
 
 
 domain = 'https://www.carsensor.net'
@@ -15,46 +15,43 @@ car_list_file = os.path.dirname(__file__) + '/carList.yaml'
 
 def crawling(cars: list[dict[str, str]], db: DBConnector):
     today = date.today()
-    car_data: list[CarData] = []
-    for car in cars:
-        url = domain + car['url']
-        while (True):
-            res = requests.get(url.replace('index.html', url))
-            if (res.status_code != 200):
-                break
-
-            soup = BeautifulSoup(res.text, 'html.parser')
-            res.close()
-
-            car_list = soup.find('div', id='carList')
-            for cassette_wrap in car_list.find_all('div', class_='cassetteWrap'):
-                price_context = cassette_wrap.find('p', class_='basePrice__content')
-                price_big_text = price_context.find('span', class_='basePrice__mainPriceNum')
-                price_small_text = price_context.find('span', class_='basePrice__subPriceNum')
-                price: int = -1
-                if (price_big_text is not None and price_small_text is not None):
-                    price = int(price_big_text.text) * 10000 + int(price_small_text.text.replace('.', '')) * 1000
-
-                detail_data = cassette_wrap.find_all('dd', class_='specList__data')
-
-                model_year_text = detail_data[0].find('span', class_='specList__emphasisData').text
-                mileage_text = detail_data[1].find('span', class_='specList__emphasisData').text
-                mileage = int(float(mileage_text) * 10000)
-
-                car_data.append(CarData(date=today, name=car['name'], price=price,
-                                        model_year=int(model_year_text), mileage=mileage))
-
-            next_button = soup.find('button', class_='pager__btn__next')
-            if ('is-disabled' in next_button.attrs['class']):
-                break
-            url = domain + re.findall(r'location.href=\'([^\'].*)\'', next_button.attrs['onclick'])[0]
-
     try:
         db.initialze()
         db.delete(today)
-        db.add(car_data)
+        for car in cars:
+            url = domain + car['url']
+            while (True):
+                res = requests.get(url.replace('index.html', url))
+                if (res.status_code != 200):
+                    break
+
+                soup = BeautifulSoup(res.text, 'html.parser')
+                res.close()
+
+                car_list = soup.find('div', id='carList')
+                for cassette_wrap in car_list.find_all('div', class_='cassetteWrap'):
+                    price_context = cassette_wrap.find('p', class_='basePrice__content')
+                    price_big_text = price_context.find('span', class_='basePrice__mainPriceNum')
+                    price_small_text = price_context.find('span', class_='basePrice__subPriceNum')
+                    price: int = -1
+                    if (price_big_text is not None and price_small_text is not None):
+                        price = int(price_big_text.text) * 10000 + int(price_small_text.text.replace('.', '')) * 1000
+
+                    detail_data = cassette_wrap.find_all('dd', class_='specList__data')
+
+                    model_year_text = detail_data[0].find('span', class_='specList__emphasisData').text
+                    mileage_text = detail_data[1].find('span', class_='specList__emphasisData').text
+                    mileage = int(float(mileage_text) * 10000)
+
+                    db.add(date=today, name=car['name'], price=price, model_year=int(model_year_text), mileage=mileage)
+
+                next_button = soup.find('button', class_='pager__btn__next')
+                if ('is-disabled' in next_button.attrs['class']):
+                    break
+                url = domain + re.findall(r'location.href=\'([^\'].*)\'', next_button.attrs['onclick'])[0]
+
         db.commit()
-    except:
+    except Exception:
         import traceback
         traceback.print_exc()
         db.rollback()
