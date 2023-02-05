96dba1e02bbdc31832f439a4d9e1425e23ad34a5
tMorriss <tmorriss@tmorriss.com>
2023-02-05 18:06:09 +0900

    add null care

--- debug.py
+++ debug.py
@@ -1,10 +1,10 @@
 from crawler import crawling, CarData
 
-name = 'CLA 45 S'
-url = '/usedcar/search.php?STID=CS210610&CARC=AM_S002&GRDKC=AM_S002_F002_K003&OPTCD=REP0'
-
 if __name__ == '__main__':
-    cars: list[dict[str, str]] = [{"name": name, "url": url}]
+    cars: list[dict[str, str]] = [
+        {'name': 'CLA 45 S', 'url': '/usedcar/search.php?STID=CS210610&CARC=AM_S002&GRDKC=AM_S002_F002_K003&OPTCD=REP0'},
+        {'name': 'C63 セダン', 'url': '/usedcar/search.php?STID=CS210610&CARC=AM_S006&GRDKC=AM_S006_F001_K001&YMIN=2019&OPTCD=REP0'}
+    ]
 
     data: list[CarData] = crawling(cars=cars)
 
